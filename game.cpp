aa27d2338d3ae027d9362f34ea138cd2414bc9d6
Pudigor <54814046+Pudigor@users.noreply.github.com>
2019-10-31 17:21:03 -0300

    Zelda
    Meu progresso no jogo até agora, algumas coisas estao meio alteradas hehexd

--- game.cpp
+++ game.cpp
@@ -1,32 +1,108 @@
-#include <allegro5/allegro.h>
-#include <iostream>
-#include "game.h"
-using namespace std;
+#include "Base.h"
 
-Game::Game(){
-    running = true;
+
+Game::Game()
+{
+    std::cout << "Initializing Game..." << std::endl;
+
+
+    playing = true;
+
+    al_init();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_init_image_addon();
+	al_init_primitives_addon();
+	al_install_keyboard();
+	al_install_audio();
+	al_init_acodec_addon();
+
+    d = al_create_display(768,672);
+    q = al_create_event_queue();
+    t = al_create_timer(1.0/30);
+
+    al_register_event_source(q, al_get_keyboard_event_source());
+	al_register_event_source(q, al_get_display_event_source(d));
+	al_register_event_source(q, al_get_timer_event_source(t));
+
+    al_start_timer(t);
+
+    InitCheck();
+
 }
 
-bool Game::onInit(){
+Game::~Game()
+{
+
+    std::cout << "Closing Game..." << std::endl;
+
+    al_uninstall_audio();
+    al_uninstall_keyboard();
+
+    al_destroy_display(d);
+    al_destroy_timer(t);
+    al_destroy_event_queue(q);
+
+    std::cout << "Game Closed!" << std::endl;
+
+}
+
+Game::InitCheck(){
+
+    std::cout << "Verifying Initialization" << std::endl;
+
     if(!al_init()) {
-      cout << "failed to initialize allegro!" << endl;
-      return false;
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+	if(!al_init_ttf_addon()) {
+		fprintf(stderr, "failed to initialize tft addon!\n");
+		return -1;
+	}
+
+	if(!al_init_image_addon()) {
+		fprintf(stderr, "failed to initialize image addon!\n");
+		return -1;
+	}
+
+	if(!al_init_acodec_addon()) {
+		fprintf(stderr, "failed to initialize acodec addon!\n");
+		return -1;
+	}
+
+	if(!al_init_primitives_addon()){
+		fprintf(stderr, "failed to initialize primitives!\n");
+        return -1;
+    }
+
+	if(!al_install_keyboard()){
+		fprintf(stderr, "failed to initialize keyboard!\n");
+		return -1;
+	}
+
+	if(!al_install_audio()){
+        fprintf(stderr, "failed to initialize audio!\n");
+		return -1;
+	}
+
+	if(!t) {
+      fprintf(stderr, "failed to create timer!\n");
+      return -1;
    }
-   return true;
-}
 
-void Game::onEvent(ALLEGRO_EVENT &evento){
-    // cuida dos eventos
-}
+	if(!q) {
+      fprintf(stderr, "failed to create event_queue!\n");
+      return -1;
+	}
 
-void Game::onLoop(){
-    // atualiza as informações a cada loop
-}
+	if(!d) {
+      fprintf(stderr, "failed to create display!\n");
+      return -1;
+   }
 
-void Game::onRender(){
-    // cuida de renderizar as informações
-}
+   std::cout << "Game Initialized Successfully!" << std::endl;
 
-bool Game::isRunning(){  // checa se o jogo está rodando
-    return this->running;
+	return 1;
+
 }
--- player.cpp
+++ player.cpp
@@ -1,21 +1,11 @@
-#include <allegro5/allegro.h>
-#include <string>
-#include "player.h"
-using namespace std;
-Player::Player(string sprite,coordenadas posicao, int dano, int vida, int id):
-Entidade(sprite,posicao, dano, vida, id){
-    this->sprite = sprite;
-    this->posicao = posicao;
-    this->dano = dano;
-    this->vida = vida;
-    this->id = id;
+#include "Player.h"
+
+Player::Player()
+{
+    //ctor
 }
 
-void Player::mover(int x, int y){
-    this->posicao.x = x;
-    this->posicao.y = y;
-}
-
-void Player::ataque(){
-    // ataca
+Player::~Player()
+{
+    //dtor
 }
