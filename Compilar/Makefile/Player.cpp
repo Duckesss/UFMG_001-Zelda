4d2e6505b1ad67e4cb1e161401b0087f65f09860
Vando Diniz <53914839+vandodiniz@users.noreply.github.com>
2019-12-11 10:50:49 -0300

    Add files via upload

--- Compilar/Makefile/Player.cpp
+++ Compilar/Makefile/Player.cpp
@@ -1,10 +1,10 @@
 #include "Header.h"
 
-Player::Load(string file_path)
-{ //cria o jogador
+Player::Load(std::string file_path)
+{
 
-//{ carregaga dados do save
-    ifstream save_file;
+//{
+    std::ifstream save_file;
     save_file.open(file_path);
 
     save_file.ignore(100, '=');
@@ -25,10 +25,10 @@ Player::Load(string file_path)
         box.yf = box.yi+47;
 
     save_file.ignore(100, '=');
-        save_file >> v.i;
+        save_file >> c.i;
 
         save_file.ignore(100, ',');
-        save_file >> v.j;
+        save_file >> c.j;
 
     save_file.ignore(100, '=');
         save_file >> progress;
@@ -44,8 +44,8 @@ Player::Load(string file_path)
     save_file.close();
 //}
 
-    //carrega os sprites do jogador
     sprite = al_load_bitmap(".\\assets\\player_sprite_0.png");
+
     att_sprite.push_back(al_load_bitmap(".\\assets\\att_sprite_0.png"));
     att_sprite.push_back(al_load_bitmap(".\\assets\\att_sprite_1.png"));
     att_sprite.push_back(al_load_bitmap(".\\assets\\att_sprite_2.png"));
@@ -62,134 +62,144 @@ Player::Load(string file_path)
     att_sprite.push_back(al_load_bitmap(".\\assets\\att_sprite_13.png"));
     att_sprite.push_back(al_load_bitmap(".\\assets\\att_sprite_14.png"));
     att_sprite.push_back(al_load_bitmap(".\\assets\\att_sprite_15.png"));
+
 }
 
 Player::MoveR(Game* game)
-{ //move o jogador para direita
-
-    //muda a posição e direção
+{
     box.BoxAddX(1);
     dir = 1;
 
-    //desfaz o movimento se o jogador colidir
     if (box.xf > 768 || game->zone[game->c.i-1][game->c.j-1].WallCheck(box))
         box.BoxAddX(-1);
 
     if (((int)box.xi)%50 <= 24)
         sprite = al_load_bitmap(".\\assets\\player_sprite_3.png");
-    else sprite = al_load_bitmap(".\\assets\\player_sprite_2.png");
+    else
+        sprite = al_load_bitmap(".\\assets\\player_sprite_2.png");
+
 }
 
 Player::MoveL(Game* game)
-{ //move o jogador para a esquerda
+{
 
-    //muda a posição e direção
     box.BoxAddX(-1);
     dir = 3;
 
-    //desfaz o movimento se o jogador colidir
     if (box.xf < 0 || game->zone[game->c.i-1][game->c.j-1].WallCheck(box))
         box.BoxAddX(1);
 
-    //animação de movimento
     if (((int)box.xi)%50 <= 24)
         sprite = al_load_bitmap(".\\assets\\player_sprite_7.png");
-    else sprite = al_load_bitmap(".\\assets\\player_sprite_6.png");
+    else
+        sprite = al_load_bitmap(".\\assets\\player_sprite_6.png");
+
 }
 
 Player::MoveU(Game* game)
-{ //move o jogador para cima
+{
 
-    //muda posição e direção
     box.BoxAddY(-1);
     dir = 2;
 
-    //desfaz o movimento se o jogador colidir
-    if (box.yi < 144 || game->zone[game->c.i-1][game->c.j-1].WallCheck(box))
+   if (box.yi < 144 || game->zone[game->c.i-1][game->c.j-1].WallCheck(box))
         box.BoxAddY(1);
 
-    //anima o movimento
     if (((int)box.yi)%50 <= 24)
-        sprite = al_load_bitmap(".\\assets\\player_sprite_5.png");
-    else sprite = al_load_bitmap(".\\assets\\player_sprite_4.png");
+        this->sprite = al_load_bitmap(".\\assets\\player_sprite_5.png");
+    else
+        this->sprite = al_load_bitmap(".\\assets\\player_sprite_4.png");
+
 }
 
 Player::MoveD(Game* game)
-{ //move o jogador para baixo
+{
 
-    //muda a posiçao e direçao do jogaodr
     box.BoxAddY(1);
     dir = 0;
 
-    //desfaz o movimento se o jogador colidir
     if (box.yf > 672 || game->zone[game->c.i-1][game->c.j-1].WallCheck(box))
         box.BoxAddY(-1);
 
-    //faz a animação
     if (((int)box.yi)%50 <= 24)
         sprite = al_load_bitmap(".\\assets\\player_sprite_1.png");
-    else sprite = al_load_bitmap(".\\assets\\player_sprite_0.png");
+    else
+        sprite = al_load_bitmap(".\\assets\\player_sprite_0.png");
 
 }
 
 Player::Attack(Game* game, Instance* instance)
-{ //realiza o ataque
+{
 
-    type = INV; //define o jogador como ivuneravel, para evitar bugs com hitbox
+    type = INV;
 
-    //cria hitboxes auxiliares
     Hitbox aux = box;
     Hitbox sw;
     int i;
 
-    //{ animação do ataque
-    sprite = att_sprite[dir*4]; //primeiro estágio de ataque
-    for(i=0; i<8; i++){ //faz a animação demorar mais
+    //{ Animação do ataque
+    sprite = att_sprite[dir*4];
+    for(i=0; i<8; i++){ //Primeiro estágio de ataque
         UpdateGame(game, instance);
         al_wait_for_event(game->q, &game->ev);
     }
 
-    sprite = att_sprite[dir*4+1]; //segundo estágio, cria uma hitbox de espada e verifica se acerta inimigos e corrige hitbox para animacao do ataque
+    sprite = att_sprite[dir*4+1];
+
     if (dir == 0){
+
         sw.xi = box.xi + 21;
         sw.xf = box.xi + 29;
         sw.yi = box.yi + 48;
         sw.yf = box.yi + 80;
+
         SwordCollision(game, sw);
+
     }
     if (dir == 1){
+
         sw.xi = box.xi + 48;
         sw.xf = box.xi + 80;
         sw.yi = box.yi + 24;
         sw.yf = box.yi + 32;
+
         SwordCollision(game, sw);
     }
     if (dir == 2){
+
         aux = box;
         box.yi -=36;
+
         sw.xi = box.xi + 15;
         sw.xf = box.xi + 23;
         sw.yi = box.yi;
         sw.yf = box.yi + 32;
+
         SwordCollision(game, sw);
+
+
     }
     if (dir == 3){
+
         aux = box;
         box.xi -=33;
+
         sw.xi = box.xi;
         sw.xf = box.xi + 32;
         sw.yi = box.yi + 24;
         sw.yf = box.yi + 32;
+
         SwordCollision(game, sw);
+
     }
-    for(i=0; i<16; i++){ //faz a animiação demorar mais
+
+    for(i=0; i<16; i++){
         UpdateGame(game, instance);
         al_wait_for_event(game->q, &game->ev);
     }
-    box = aux; //desfaz alterações da hitbox
+    box = aux;
 
-    sprite = att_sprite[dir*4+2]; //terceiro estágio de ataque
-    //corrige hitbox para animacao do ataque
+    sprite = att_sprite[dir*4+2];
     if (dir == 2){
       aux = box;
       box.yi -=33;
@@ -198,15 +208,13 @@ Player::Attack(Game* game, Instance* instance)
       aux = box;
       box.xi -=21;
     }
-
-    for(i=0; i<8; i++){ //faz a animiação demorar mais
+    for(i=0; i<8; i++){
         UpdateGame(game, instance);
         al_wait_for_event(game->q, &game->ev);
     }
-    box = aux; //desfaz alterações da hitbox
+    box = aux;
 
-    sprite = att_sprite[dir*4+3]; //quarto estágio de ataque
-    //corrige hitbox para animacao do ataque
+    sprite = att_sprite[dir*4+3];
     if (dir == 2){
       aux = box;
       box.yi -=9;
@@ -215,15 +223,13 @@ Player::Attack(Game* game, Instance* instance)
       aux = box;
       box.xi -=9;
     }
-
-    for(i=0; i<8; i++){ //faz a animiação demorar mais
+    for(i=0; i<8; i++){
         UpdateGame(game, instance);
         al_wait_for_event(game->q, &game->ev);
     }
-    box = aux; //desfaz alterações da hitbox
+    box = aux;
     //}
 
-    //retorna o jogador para como estava antes
     if(dir == 1)
         sprite = sprite = al_load_bitmap(".\\assets\\player_sprite_2.png");
     if(dir == 3)
@@ -232,28 +238,29 @@ Player::Attack(Game* game, Instance* instance)
         sprite = sprite = al_load_bitmap(".\\assets\\player_sprite_4.png");
     if(dir == 0)
         sprite = sprite = al_load_bitmap(".\\assets\\player_sprite_0.png");
+
     UpdateGame(game, instance);
     al_wait_for_event(game->q, &game->ev);
+
     type = 0;
 
-    //ativa o cooldown do ataque
     cd = 50;
+
 }
 
 Player::Save(Game* game)
-{ //salva o progrsso do jogador
+{
 
-    //localiza o save do jogador
     std::ofstream file;
     if (save == 0)
-        file.open(".\\assets\\save_0.txt");
+        file.open(".\\entities\\save_0.txt");
     else if (save == 1)
-        file.open(".\\assets\\save_1.txt");
+        file.open(".\\entities\\save_1.txt");
     else if (save == 2)
-        file.open(".\\assets\\save_2.txt");
-    else return 0;
+        file.open(".\\entities\\save_2.txt");
+    else
+        return 0;
 
-    // caso ele esteja na zona do boss, corrige isso
     if(game->c.i == 1 && game->c.j == 1){
         game->c.i = 2;
         box.xi = 384;
@@ -262,32 +269,31 @@ Player::Save(Game* game)
         box.yf = box.yi + 47;
     }
 
-    //escreve no arquivo
-    file << "registered = 1" << endl;
-    file << "name = " << name << endl;
-    file << "health = " << health << endl;
-    file << "hitbox = " << box.xi << ", " << box.yi << endl;
-    file << "zone = " << game->c.i << ", " << game->c.j << endl;
-    file << "progress =  " << progress << endl;
-    file << "arrow = " << a << endl;
-    file << "rupee = " << r << endl;
+    file << "registered = 1" << std::endl;
+    file << "name = " << name << std::endl;
+    file << "health = " << health << std::endl;
+    file << "hitbox = " << box.xi << ", " << box.yi << std::endl;
+    file << "zone = " << game->c.i << ", " << game->c.j << std:: endl;
+    file << "progress =  " << progress << std::endl;
+    file << "arrow = " << a << std::endl;
+    file << "rupee = " << r << std::endl;
+
     file.close();
+
 }
 
 Player::SwordCollision(Game* game, Hitbox sw)
-{ //faz a colisão da espada do jogador
-    //percorre o vetor de entidades
+{
     for(int i = 0; i<game->zone[game->c.i-1][game->c.j-1].e.size(); i++){
-        //se for um inimigo comum, desabilita ele e cria um drop
         if(sw.Collision(game->zone[game->c.i-1][game->c.j-1].e[i]->box) && game->zone[game->c.i-1][game->c.j-1].e[i]->type == TYPE_ENEMY){
             game->zone[game->c.i-1][game->c.j-1].e[i]->type = 0;
             Drop* d = new Drop(game->zone[game->c.i-1][game->c.j-1].e[i]->box);
             game->zone[game->c.i-1][game->c.j-1].e.push_back(d);
         }
-
-        //se for um boss, diminui a vida dele
         if(sw.Collision(game->zone[game->c.i-1][game->c.j-1].e[i]->box) && game->zone[game->c.i-1][game->c.j-1].e[i]->type == TYPE_BOSS){
             game->zone[game->c.i-1][game->c.j-1].e[i]->health--;
         }
     }
 }
+
+
--- Compilar/Makefile/Zelda.cpp
+++ Compilar/Makefile/Zelda.cpp
@@ -1,8 +1,13 @@
 #include "Header.h"
 
-void Check(Game* game, Instance* Instance){ //garante que algumas variáveis não vão ultrapassar seu limite
+void out(){
 
-    //garante que não vai tentar acessar um valor que não está na matriz
+    std::cout << 'a' << std::endl;
+
+}
+
+void Check(Game* game, Instance* Instance){
+
     if(game->c.i > 3)
         game->c.i = 3;
     if(game->c.i < 1)
@@ -12,7 +17,6 @@ void Check(Game* game, Instance* Instance){ //garante que algumas variáveis não 
     if(game->c.j < 1)
         game->c.j = 1;
 
-    //evita valores que são maiores que a HUD suporta
     if(game->player->health > 16)
         game->player->health = 16;
     if(game->player->a > 999)
@@ -27,9 +31,9 @@ void Check(Game* game, Instance* Instance){ //garante que algumas variáveis não 
 
 }
 
-void UpdateGame(Game* game, Instance* instance){ //atualiza o jogo
+void UpdateGame(Game* game, Instance* instance){ //Atualiza jogo
 
-    //{atualiza os cooldowns do jogador
+    //{ Atualiza cooldowns
     game->player->cd--;
     game->player->cd_2--;
     if(game->player->cd<0)
@@ -38,44 +42,41 @@ void UpdateGame(Game* game, Instance* instance){ //atualiza o jogo
         game->player->cd_2 = 0;
     //}
 
-    //desenha a tela
     al_draw_bitmap(instance->map_background, 0 - (game->c.j - 1)*MAP_X, 144 - (game->c.i - 1)*MAP_Y, 0);
     al_draw_bitmap(instance->hud, 0, 0, 0);
     al_draw_bitmap(game->player->sprite, game->player->box.xi, game->player->box.yi, 0);
 
-    //atualiza as entidades e as desenha se forem validas
-    for(int i=0; i<game->zone[game->c.i-1][game->c.j-1].e.size(); i++){ //percorrendo o vetor de entidades da zona atual
+    //{ Atualiza entidades
+    for(int i=0; i<game->zone[game->c.i-1][game->c.j-1].e.size(); i++){
 
-        if(game->zone[game->c.i-1][game->c.j-1].e[i]->type !=0) // se a entidade ainda for valida, atualiza ela
+        if(game->zone[game->c.i-1][game->c.j-1].e[i]->type !=0)
             game->zone[game->c.i-1][game->c.j-1].e[i]->Update(game, instance);
 
-        if(game->zone[game->c.i-1][game->c.j-1].e[i]->type !=0 && game->zone[game->c.i-1][game->c.j-1].e[i]->type != TYPE_BOW)// se a entidade ainda for valida, desenha ela
+        if(game->zone[game->c.i-1][game->c.j-1].e[i]->type !=0 && game->zone[game->c.i-1][game->c.j-1].e[i]->type != TYPE_BOW)
             al_draw_bitmap(game->zone[game->c.i-1][game->c.j-1].e[i]->sprite, game->zone[game->c.i-1][game->c.j-1].e[i]->box.xi, game->zone[game->c.i-1][game->c.j-1].e[i]->box.yi, 0);
 
     }
+    //}
 
-    //atualiza informações do jogo
     instance->DisplayHealth(game->player->health, 435, 72);
     instance->DisplayNum(game->player->r, 195, 24);
     if(game->player->progress == 1)
         instance->DisplayNum(game->player->a, 195, 72);
-    al_flip_display();
 
-    //checa consistencia dos dados
     Check(game, instance);
+    al_flip_display();
 
 }
 
-int main(){ //main
+int main(){
 
-    srand(time(0)); //da set em uma nova seed para ser aleatorio
+    srand(time(0));
 
-    Game game = Game(); //cria o jogo
-    Instance instance = Instance(); //cria uma classe instance
+    Game game = Game();
+    Instance instance = Instance();
 
-    instance.StartMenu(&game); //Start o menu principal
+    instance.StartMenu(&game);
 
-    //finaliza o jogo e livra as memorias
     delete &instance;
     delete &game;
 
